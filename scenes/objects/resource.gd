85b53b8ace20b47475980a7d0f37467165a39b9d
Marek Belski <marek.belski@gmail.com>
2025-09-21 00:36:57 -0700

    More balancing to make things more easy/fun.

--- scenes/objects/resource.gd
+++ scenes/objects/resource.gd
@@ -1,7 +1,9 @@
 extends Area2D
 
+@export var resource_amount : int = 1
+
 func _on_body_entered(body):
 	if body.has_method("add_resource"):
-		if body.add_resource(4):
+		if body.add_resource(resource_amount):
 			GameEvents.resource_collected.emit()
 			queue_free()
