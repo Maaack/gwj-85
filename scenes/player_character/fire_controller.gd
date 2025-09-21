07e1fa2df0672893963f12a055d0ac2119644e68
Marek Belski <marek.belski@gmail.com>
2025-09-21 08:47:23 -0700

    Adds shooting sounds to player.

--- scenes/player_character/fire_controller.gd
+++ scenes/player_character/fire_controller.gd
@@ -3,6 +3,7 @@ extends Node
 @export_node_path("CharacterBody2D") var character_body_node_path : NodePath = ^".."
 @export var player_controller : PlayerController
 @export var ammo_storage : UnitStorageComponent
+@export var fire_sound_player : AudioStreamPlayer
 @export var no_ammo_sound_player : AudioStreamPlayer
 @export var bullet_scene : PackedScene
 @export var spawn_offset : Vector2 = Vector2.ZERO
@@ -16,8 +17,8 @@ var in_cooldown : bool = false
 
 func _no_ammo() -> void:
 	if no_ammo_sound_player:
-		no_ammo_sound_player.play()
-
+		if not no_ammo_sound_player.playing:
+			no_ammo_sound_player.play()
 
 func _process(_delta) -> void:
 	if not Input.is_action_pressed(fire_action): return
@@ -30,6 +31,8 @@ func _process(_delta) -> void:
 	bullet_instance.global_position = character_body.global_position + spawn_offset.rotated(player_controller.facing_vector.angle())
 	bullet_instance.velocity = spawn_speed * player_controller.facing_vector
 	ammo_storage.subtract(1)
+	if fire_sound_player:
+		fire_sound_player.play()
 	GameEvents.object_spawned.emit(bullet_instance)
 	in_cooldown = true
 	await get_tree().create_timer(cooldown_delay, false).timeout
